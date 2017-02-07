9eaec2541f0c0f2d92b83fc5ff3102e146af786c
GrayEmbrace <frostboy16@gmail.com>
2017-02-07 13:11:22 -0800

    feb 7 branch merge
    take 1

--- TuckersTemple/Assets/Scripts/GameMaster.cs
+++ TuckersTemple/Assets/Scripts/GameMaster.cs
@@ -14,26 +14,16 @@ public class GameMaster : MonoBehaviour
 {
     // public fields:
     public float slideSpeed = .02f;
-<<<<<<< HEAD
-	public GameObject outerWall;
-	public GameObject Tile; //The tile prefab to spawn in
-	public GameObject Character;
-	public GameObject Trap;
-	public GameObject Enemy;
-	public float tileSize; //the size of the tile prefab(should be square)
-	public int numRows = 2; //number of tiles to size
-	public int numCols = 2;
-=======
     public GameObject outerWall;
     public GameObject Tile; //The tile prefab to spawn in
     public GameObject Character;
     public GameObject Trap;
     public GameObject Enemy;
+    public GameObject Goal;
     public float tileSize; //the size of the tile prefab(should be square)
     public int numRows = 2; //number of tiles to size
     public int numCols = 2;
     public Canvas winScreen;
->>>>>>> master
 
     // private fields:
     private const int N = 0;
@@ -50,12 +40,8 @@ public class GameMaster : MonoBehaviour
     private float totalOffset = 0; //holds total offset for a move, to keep it locked to 1 tile away
     private GameObject[][] tileGrid; // the holder for all the tiles
     private GameObject roy; //Roy is private, he just likes it that way
-<<<<<<< HEAD
-	private GameObject enemy; 
-=======
+	private GameObject enemy; // enemies also keep private affairs, right?
     private GameObject goal;
-	//private GameObject enemy; // enemies also keep private affairs, right?
->>>>>>> master
     private bool canInputMove = true;
     private bool charsWalking = false;
     private bool tilesSliding = false;
@@ -89,14 +75,8 @@ public class GameMaster : MonoBehaviour
         roy = Instantiate(Character, new Vector3(tileGrid[0][0].transform.position.x, tileGrid[0][0].transform.position.y, tileGrid[0][0].transform.position.z), Quaternion.identity, tileGrid[0][0].transform);
         actors.Add(roy);
         goal = Instantiate(Goal, new Vector3(tileGrid[1][1].transform.position.x, tileGrid[1][1].transform.position.y, tileGrid[1][1].transform.position.z), Quaternion.identity, tileGrid[1][1].transform);
-
-<<<<<<< HEAD
 		enemy = Instantiate(Enemy, new Vector3(tileGrid[1][0].transform.position.x, tileGrid[0][0].transform.position.y, tileGrid[0][0].transform.position.z), Quaternion.identity, tileGrid[0][0].transform);
 		actors.Add(enemy);
-=======
-		//enemy = Instantiate(Character, new Vector3(tileGrid[2][0].transform.position.x, tileGrid[0][0].transform.position.y, tileGrid[0][0].transform.position.z), Quaternion.identity, tileGrid[0][0].transform);
-		//actors.Add(enemy);
->>>>>>> master
 
         //Add in outer walls to the grid
         outerWall = Instantiate(outerWall, Vector3.zero, Quaternion.identity);
@@ -154,11 +134,7 @@ public class GameMaster : MonoBehaviour
                 foreach(GameObject actor in actors)
                 {
                     actor.GetComponent<Actor>().walk();
-<<<<<<< HEAD
 					//actor.GetComponent<Enemy>().walk();
-=======
-		    // actor.GetComponent<Enemy>().walk();
->>>>>>> master
                 }
                 moves++;
             }   
