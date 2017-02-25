cba0c3aecb9863ae30867edf332722fad6562812
ghostofrasputin <jalprest@ucsc.edu>
2017-02-24 23:26:32 -0800

    code edits, file cleanup

--- TuckersTemple/Assets/Scripts/Menu.cs
+++ TuckersTemple/Assets/Scripts/Menu.cs
@@ -12,9 +12,13 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Menu : MonoBehaviour {
-
+	public GameObject menuSlide1;
+	private Animator anim;
+	private int counter = 0;
 	// Use this for initialization
 	void Start () {
+		anim = menuSlide1.GetComponent<Animator>();
+		Time.timeScale = 1;
 	}
 	
 	// We might have something
@@ -27,5 +31,40 @@ public class Menu : MonoBehaviour {
 	public void loadScene(string scene)
 	{
 		SceneManager.LoadScene(scene);
+	}
+
+	// controls main menu shift controls:
+	public void shift(string flag){
+		anim.enabled = true;
+		if (flag == "right") {
+			counter++;
+			if (counter == 1) {
+				anim.Play ("menuSlide1");
+			}
+			if (counter == 2) {
+				anim.Play ("menuSlide2");
+			}
+			if (counter == 3) {
+				anim.Play ("menuSlide3");
+			}
+			if (counter == -1) {
+				anim.Play ("menuSlide4");
+			}
+		}
+		if (flag == "left") {
+			counter--;
+			if (counter == 1) {
+				anim.Play ("menuSlide5");
+			}
+			if (counter == 2) {
+				anim.Play ("menuSlide6");
+			}
+			if (counter == 3) {
+				anim.Play ("menuSlide7");
+			}
+			if (counter == -1) {
+				anim.Play ("menuSlide8");
+			}
+		}
 	}
 }
