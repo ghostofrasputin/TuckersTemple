ec344b9f5a130d10d13c0489da4d1232b93bc675
GrayEmbrace <frostboy16@gmail.com>
2017-05-20 20:12:44 -0700

    hint arrow revamp

--- TuckersTemple/Assets/HintArrow.cs
+++ TuckersTemple/Assets/HintArrow.cs
@@ -10,28 +10,30 @@ public class HintArrow : MonoBehaviour {
     private float step;
     private float time;
     private SpriteRenderer sr;
+    private GameObject gm;
 
 	// Use this for initialization
 	void Start () {
         sr = GetComponent<SpriteRenderer>();
+        gm = GameObject.FindGameObjectWithTag("GameController");
         sr.enabled = false;
         startPos = transform.position;
-        goalPos = startPos + new Vector2(1.5f,0);
-        speed = .5f;
+        goalPos = startPos + new Vector2(0,3.0f);
+        speed = .2f;
         step = speed * Time.deltaTime;
         time = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+        if(gm.GetComponent<GameMasterFSM>().currentLevel >= 3 || Input.touchCount > 0 || Input.GetMouseButtonDown(0))
         {
             time = 0f;
             sr.enabled = false;
             transform.position = startPos;
         }
         time += Time.deltaTime;
-        if (time > 8f)
+        if (time > 4f)
         {
             sr.enabled = true;
             transform.position = Vector2.MoveTowards(transform.position, goalPos, step);
