eaeaa0142da62d7e2c3f5195b47aebcd9988d21f
Andrew C <acsky713@gmail.com>
2017-06-08 22:26:35 -0700

    Google Play Services

---
+++ TuckersTemple/Assets/Scripts/SocialPlatform.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+
+public class SocialPlatform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+
+        PlayGamesPlatform.Instance.Authenticate(SignInCallback, true);
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SignInCallback(bool success)
+    {
+        if (success)
+        {
+            Debug.Log("(TuckersTemple) Signed in!");
+        }
+        else
+        {
+            Debug.Log("(TuckersTemple) Sign-in failed...");
+        }
+    }
+
+    public void ShowAchievements()
+    {
+        if (PlayGamesPlatform.Instance.localUser.authenticated)
+        {
+            PlayGamesPlatform.Instance.ShowAchievementsUI();
+        } else
+        {
+            Debug.Log("Cannot Show Achievements, not logged in");
+        }
+    }
+}
